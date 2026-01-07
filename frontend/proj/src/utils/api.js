a35803ca71894671a590cfa21a0f534a1ba5a2d7
JagmeetSingh9 <jagmeetsingh999991@gmail.com>
2026-01-07 22:08:44 +0530

    Fixing MongoDB URI

--- frontend/proj/src/utils/api.js
+++ frontend/proj/src/utils/api.js
@@ -1,7 +1,7 @@
 // frontend/proj/src/utils/api.js
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL ='http://localhost:3000';
 
 const api = axios.create({
   baseURL: API_URL,
